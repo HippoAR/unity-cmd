bafa09cd098c4998ca6ee735312ec1b844dc7817
Zehao Li <qft.gtr@gmail.com>
2018-03-23 00:52:57 -0700

    build

--- build.sh
+++ build.sh
@@ -3,12 +3,16 @@
 project="xx"
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -serial UNITY_SERIAL_NUMBER \
+  -username UNITY_USERNAME \
+  -password UNITY_PASSWORD \
+  -projectPath . \
+  -executeMethod MyBuild.BuildIOS \
   -quit
 
 echo 'Logs from build'
